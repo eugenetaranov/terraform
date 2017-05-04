a3c5a491eb9c12fc4dfb0ee2cbb021ba0dbe3a3a
Eugene Taranov <eugene@taranov.me>
2017-05-04 02:21:38 +0200

    pulled updates from gophercloud

--- vendor/github.com/eugenetaranov/gophercloud/openstack/client.go
+++ vendor/github.com/eugenetaranov/gophercloud/openstack/client.go
@@ -302,7 +302,7 @@ func NewOrchestrationV1(client *gophercloud.ProviderClient, eo gophercloud.Endpo
 
 // NewDBV1 creates a ServiceClient that may be used to access the v1 DB service.
 func NewDBV1(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
-	eo.ApplyDefaults("database")
+	eo.ApplyDefaults("rax:database")
 	url, err := client.EndpointLocator(eo)
 	if err != nil {
 		return nil, err
